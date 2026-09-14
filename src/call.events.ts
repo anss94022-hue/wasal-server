7aa7fcb39b838de528e98deff792ede0aed635aa
anss94022-hue <anss94022@gmail.com>
2026-09-14 21:39:39 +0300

    Delete src/call.events.ts

--- src/call.events.ts
+++
@@ -1,9 +0,0 @@
-export const CALL_EVENTS = {
-  INCOMING: "call:incoming",
-  ACCEPT: "call:accept",
-  REJECT: "call:reject",
-  END: "call:end",
-  OFFER: "call:offer",
-  ANSWER: "call:answer",
-  ICE_CANDIDATE: "call:ice-candidate",
-} as const;
